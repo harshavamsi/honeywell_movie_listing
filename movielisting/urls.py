eafd09b440a497486107c3c13d8359910e44ae8e
harshavamsi <harshavamsi096@gmail.com>
2017-11-15 19:57:14 +0530

    fixed search

--- movielisting/urls.py
+++ movielisting/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search/$', views.search, name='search'),
     url(r'^(?P<movie_title>.*)/$', views.detail, name="detail" ),
 ]
--- movielisting/views.py
+++ movielisting/views.py
@@ -4,6 +4,14 @@ from django.http import HttpResponse
 
 from movielisting.models import Movie
 
+def search(request):
+    if request.method == 'POST':
+        query = request.POST["search"]
+        movies = Movie.objects.filter(movie_title__icontains=query)
+        context = {'all_movies' : movies }
+        return render(request,'movies/index.html',context)
+    return HttpResponseRedirect(reverse('index'))
+
 def index(request):
     all_movies = Movie.objects.order_by('movie_title')
     context = {'all_movies' : all_movies}
@@ -13,10 +21,3 @@ def detail(request, movie_title):
     movie = get_object_or_404(Movie, movie_title=movie_title)
     context = {'movie' : movie}
     return render(request,'movies/details.html',context)
-
-
-def search(request):
-	if request.method == 'POST':
-		query = request.POST["search"]
-		posts = Post.objects.filter(movie_title__icontains=query)
-		jsonDec = json.decoder.JSONDecoder()
